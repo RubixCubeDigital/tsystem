947c20678a9b7687b87e7489b1435a3dc230ee07
Themba <themba.dev@gmail.com>
2014-12-14 08:31:01 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tsystem::Application.config.secret_key_base = '9ab07049d9b323f713f9485b3fd1e4bf9de0f8df190a76c6873baa042c06b65639d696aede36e688a44dfbc2ee14d051139071c01992d1f9901ddee275911b7f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tsystem::Application.config.session_store :cookie_store, key: '_tsystem_session'
