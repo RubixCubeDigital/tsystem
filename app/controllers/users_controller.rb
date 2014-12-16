0f6cf7d6cd048ff3260e37706e6151f84ebc5f3c
Themba <themba.dev@gmail.com>
2014-12-16 20:26:34 +0200

    Added a profile update functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,25 @@ class UsersController < ApplicationController
   end
 
   def show
+  	@user = User.find(params[:id])
+  end
+
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  def update
+  	@user = User.find(params[:id])
+  	@user.update(user_params)
+
+  	if @user.update(user_params)
+  		flash[:notice] = 'Profile has been updated.'
+  		redirect_to @user
+  	else
+  		flash[:alert] = 'Profile has not been updated.'
+
+  		render 'edit'
+  	end	
   end
 
   private
